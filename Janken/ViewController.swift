a3ed371f03adf7cdaf0df47b3f382ee75ecf7ca6
Ashika <ashibasama@gmail.com>
2023-04-06 06:55:34 +0900

    紐付け

--- Janken/ViewController.swift
+++ Janken/ViewController.swift
@@ -14,6 +14,11 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBOutlet weak var answerImageView: UIImageView!
+    
+    @IBOutlet weak var answerLabel: UILabel!
+    
+    @IBAction func shuffleAction(_ sender: Any) {
+    }
 }
 
